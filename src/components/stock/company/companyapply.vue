80846c6696fb67f1cc4a3ab0de60b32d871c83a6
yunwisdom <qq695926920@163.com>
2021-06-21 15:41:45 +0800

    init

--- src/components/stock/company/companyapply.vue
+++ src/components/stock/company/companyapply.vue
@@ -82,8 +82,8 @@
                       <span style="position:relative;" ><span style="color:red;margin-right:0px;position:absolute;left:-10px;top:0px;">*</span>所属行业</span>
                     </a-col>
                     <a-col :span="8">
-                      <a-select mode="multiple" v-model="company.territory"  @blur="validFieldToast('territory')"  placeholder="请选择所属行业！" style="width:100%; border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0;">
-                        <template v-for="(elem,index) in territoryList ">
+                      <a-select mode="single" v-model="company.territory"  @blur="validFieldToast('territory')"  placeholder="请选择所属行业！" style="width:100%; border: 0px solid #fefefe;  border-bottom: 1px solid #f0f0f0;">
+                        <template v-for="(elem,index) in industryList ">
                           <a-select-option :key="elem" :index="index" :value="elem">
                             {{elem}}
                           </a-select-option>
@@ -410,7 +410,7 @@ export default {
       acceptType: workconfig.compcolumns.acceptType,
       commonTypeColumns: workconfig.compcolumns.commonTypeColumns,
       sealTypeColumns: workconfig.compcolumns.sealTypeColumns,
-      territoryList:[ '房地产行业', '物业行业', '商管行业', '金融行业', '商贸行业', '建筑行业', '高新技术行业', '监理行业', '医疗行业', '商务咨询行业', '环保行业', '教育行业'],
+      industryList:[ '房地产行业', '物业行业', '商管行业', '金融行业', '商贸行业', '建筑行业', '高新技术行业', '监理行业', '医疗行业', '商务咨询行业', '环保行业', '教育行业'],
       breadcrumb:[{icon:'home',text:'首页',path:'/stock/workspace'},{icon:'user',text:'工商管理',path:'/stock/workspace'},{icon:'form',text:'设立公司申请',path:''}],
       statusType:{'valid':'有效','invalid':'删除'},
       zoneType:{'领地集团总部':'领地集团总部','成渝区域':'成渝区域','两湖区域':'两湖区域','川北区域':'川北区域','乐眉区域':'乐眉区域','中原区域':'中原区域','攀西区域':'攀西区域','新疆区域':'新疆区域','大湾区域':'大湾区域','北京区域':'北京区域'},
